312aca76f7588c798f8df3245e3c576ac18e0233
adrummond-hl <143145478+adrummond-hl@users.noreply.github.com>
2025-02-28 11:37:55 -0700

    use latest version of hiddenlayer sdk

--- internal/dbx/notebooks/hl_scan_model.py
+++ internal/dbx/notebooks/hl_scan_model.py
@@ -37,7 +37,7 @@ from IPython.display import display, Javascript
 
 if not importlib.util.find_spec("hiddenlayer"):
     # same as "%pip install" but we can't do that within an if statement
-    get_ipython().run_line_magic('pip', 'install hiddenlayer-sdk==2.0.2')
+    get_ipython().run_line_magic('pip', 'install hiddenlayer-sdk==2.0.3')
     # same as "%restart_python" but we can't do that within an if statement
     display(Javascript('Jupyter.notebook.kernel.restart()'))
 
